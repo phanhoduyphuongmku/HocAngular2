5cfc4be1dbe9c4300a33323e1a65ba0e162c50e8
phanhoduyphuong <phanhoduyphuong@gmail.com>
2018-03-20 14:26:43 +0700

    binding event

--- src/app/word/word.component.ts
+++ src/app/word/word.component.ts
@@ -10,4 +10,9 @@ import { Component } from '@angular/core';
 export class WordComponent{
     en:string = 'Hello';
     vn:string = 'Xin chao';
+    imgUrl = 'https://s.vnecdn.net/vnexpress/restruct/i/v55/graphics/img_logo_vne_web.gif'
+    forgot = false;
+    toggleForgot(){
+        this.forgot = !this.forgot;
+    }
 }
